faa51bb530ec2557642d12ee2a0c970de5c9da95
Nick Martinovic <nmartinovic@gmail.com>
2020-04-07 08:40:40 +0900

    Continue work on AR1

--- SQL.4_Active_record_under_the_hood/AR1/lib/01_sql_object.rb
+++ SQL.4_Active_record_under_the_hood/AR1/lib/01_sql_object.rb
@@ -5,10 +5,23 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    if @columns == nil
+      @columns = DBConnection.execute2(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{self.table_name}
+      SQL
+      @columns = @columns[0].map { |ele| ele.to_sym}
+    end
+    @columns
   end
 
   def self.finalize!
+    self.columns.each do |column|
+      define_method(column) {attributes[column]}
+      define_method("#{column}=") { |val| attributes[column]=val}
+    end
   end
 
   def self.table_name=(table_name)
@@ -32,11 +45,18 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |key,value|
+      key = key.to_sym
+      if !self.class.columns.include?(key)
+        raise "unknown attribute '#{key}'"
+      end
+      self.send "#{key}=", value
+    end
+
   end
 
   def attributes
-    # ...
+    @attributes ||= @attributes = Hash.new
   end
 
   def attribute_values
