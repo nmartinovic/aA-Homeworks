cc8ad995317b5ae592af30cca965a7ee52d6486f
Nick Martinovic <nmartinovic@gmail.com>
2020-04-27 08:05:43 +0900

    Building cat#new view

--- Rails/cats99/app/controllers/cats_controller.rb
+++ Rails/cats99/app/controllers/cats_controller.rb
@@ -1,6 +1,29 @@
 class CatsController < ApplicationController
 
     def index
+        @cats = Cat.all
+        render :index
+    end
 
+    def show
+        @cat = Cat.find_by(id: params[:id])
+        render :show
+    end
+
+    def new
+        render :new
+    end
+
+    def edit
+    end
+
+    def create
+        render json: "Creating cat"
+    end
+
+
+    private
+    def cat_params
+        params.require(:cat).permit(:birth_date, :color, :sex, :name, :description)
     end
 end
