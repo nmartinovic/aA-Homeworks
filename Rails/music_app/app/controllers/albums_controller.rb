e8a00b98a4d7ca8003fc53004ad5b74c66ec9dd7
Nick Martinovic <nmartinovic@gmail.com>
2020-06-02 08:20:54 +0900

    Working on albums views

--- Rails/music_app/app/controllers/albums_controller.rb
+++ Rails/music_app/app/controllers/albums_controller.rb
@@ -2,7 +2,46 @@ class AlbumsController < ApplicationController
 
     def new
         @band = Band.find_by(id: params[:band_id])
-        render json: "Create a new album for #{@band.name}"
+        render :new
 
+    end
+
+    def create
+        @album = Album.new(album_params)
+        if @album.save
+            render json: "#{@album.name} is saved!"
+        else
+            render json: "ERROR!!!"
+        end
+    end
+
+    def edit
+        @album = Album.find_by(id: params[:id])
+        render json: "Replace this with an edit template for #{@album.name}"
+    end
+
+    def show
+        @album = Album.find_by(id: params[:id])
+        render json: "This is the show page for #{@album.name}"
+    end
+
+    def update
+        @album = Album.find_by(id: params[:id])
+        if @album.update_attributes(album_params)
+            render json: @album.attributes
+        else
+            render json: "This caused an error"
+        end
+    end
+
+    def destroy
+        @album = Album.find_by(id: params[:id])
+        @album.destroy
+    end
+
+    private
+
+    def album_params
+        params.require(:album).permit(:name, :band_id, :year, :studio?)
     end
 end
