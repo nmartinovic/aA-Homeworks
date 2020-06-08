f184ae6330f08409444862b787796abd212337d5
Nick Martinovic <nmartinovic@gmail.com>
2020-06-09 07:32:24 +0900

    added TracksController

--- Rails/music_app/app/models/track.rb
+++ Rails/music_app/app/models/track.rb
@@ -3,3 +3,13 @@ class Track < ApplicationRecord
     belongs_to :album
     has_one :band, through: :album
 end
+
+
+# - Taylor Swift (Band)
+# ---- Red (Album)
+# -------- 22 (Song)
+# -------- Begin Again (Song)
+# ---- 1989 (Album)
+# -------- Shake it off (Song)
+# -------- Bad Blood (Song)
+# - Tim McGraw (Band)
