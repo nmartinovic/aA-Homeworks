3f91cf3c6a9ea8d7e1aaea31319562bcaa08acd7
Nick Martinovic <nmartinovic@gmail.com>
2020-06-20 11:49:55 +0900

    Finished the user model

--- Rails/goals_app/README.md
+++ Rails/goals_app/README.md
@@ -1,5 +1,9 @@
 # README
 
+ACTIONS NEEDED:
+- Add index to users migration
+
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
---
+++ Rails/goals_app/app/models/user.rb
@@ -0,0 +1,43 @@
+
+class User < ApplicationRecord
+        attr_reader :password
+
+        validates :email, null:false
+        validates :password, length: { minimum: 6, allow_nil: true}
+
+        after_initialize :ensure_session_token
+    # t.string :email, null: false
+    # t.string :password_digest, null: false
+    # t.string :session_token, null: false
+
+    def self.find_by_credentials(email, password)
+        @user = User.find_by(email: email)
+        if @user.is_password?(password)
+            @user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def generate_unique_session_token
+        SecureRandom::urlsafe_base64
+    end
+end
