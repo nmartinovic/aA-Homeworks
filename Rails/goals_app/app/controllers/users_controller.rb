23ddc96bb79e781c1606b1c5e3b720947441cdce
Nick Martinovic <nmartinovic@gmail.com>
2020-06-22 06:16:44 +0900

    Finished the authentication

--- Rails/goals_app/app/controllers/users_controller.rb
+++ Rails/goals_app/app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
 
+    def new
+        render :new
+    end
 
+    def create
+        @user = User.new(email:params["user"]["email"],password:params["user"]["password"])
+        if @user.valid?
+            @user.save
+            login!(@user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    def show
+        render :show
+    end
 end
