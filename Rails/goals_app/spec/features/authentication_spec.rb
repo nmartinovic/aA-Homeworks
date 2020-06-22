371133f521297bf37cecca02f51b91eba77dcc26
Nick Martinovic <nmartinovic@gmail.com>
2020-06-23 07:26:00 +0900

    finished phase 1

--- Rails/goals_app/spec/features/authentication_spec.rb
+++ Rails/goals_app/spec/features/authentication_spec.rb
@@ -3,23 +3,68 @@ require 'spec_helper'
 require 'rails_helper'
 
 feature 'the signup process' do
-  scenario 'has a new user page'
+  scenario 'has a new user page' do
+    visit new_user_url
+    expect(page).to have_content 'Create a User'
+  end
 
   feature 'signing up a user' do
-
-    scenario 'shows username on the homepage after signup'
+    before(:each) do
+      visit new_user_url
+      fill_in 'Enter email', with: 'testingusername'
+      fill_in 'Enter Password', with: 'testingpassword'
+      click_on 'Submit'
+    end
+    scenario 'shows username on the homepage after signup' do
+      expect(page).to have_content 'testingusername'
+    end
 
   end
 end
 
 feature 'logging in' do
-  scenario 'shows username on the homepage after login'
+  before(:each) do
+      visit new_user_url
+      fill_in 'Enter email', with: 'testingusername'
+      fill_in 'Enter Password', with: 'testingpassword'
+      click_on 'Submit'
+      click_on 'Logout'
+  end
+  scenario 'shows username on the homepage after login' do
+      visit new_session_url
+      fill_in 'Enter email', with: 'testingusername'
+      fill_in 'Enter Password', with: 'testingpassword'
+      click_on 'Log In'
+      expect(page).to have_content 'testingusername'
+  end
 
 end
 
 feature 'logging out' do
-  scenario 'begins with a logged out state'
+  before(:each) do
+    visit new_user_url
+    fill_in 'Enter email', with: 'testingusername'
+    fill_in 'Enter Password', with: 'testingpassword'
+    click_on 'Submit'
+    click_on 'Logout'
+  end
+  scenario 'begins with a logged out state' do
+      visit new_session_url
+      fill_in 'Enter email', with: 'testingusername'
+      fill_in 'Enter Password', with: 'testingpassword'
+      click_on 'Log In'
+      click_on 'Logout'
+      expect(page).not_to have_content 'testingusername'
 
-  scenario 'doesn\'t show username on the homepage after logout'
+  end
+
+  scenario 'doesn\'t show username on the homepage after logout' do
+      visit new_session_url
+      fill_in 'Enter email', with: 'testingusername'
+      fill_in 'Enter Password', with: 'testingpassword'
+      click_on 'Log In'
+      click_on 'Logout'
+      expect(page).not_to have_content 'testingusername'
+  end
 
 end
